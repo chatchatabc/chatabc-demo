980cf94955c8bdd33ef112ff53f1f598fa2870dc
Libing Chen <libing.chen@gmail.com>
2023-03-08 15:49:33 +0800

    app: ChatabcAdminApplication

---
+++ chatabc-admin/src/main/java/com/chatchatabc/chatabc/admin/ChatabcAdminApplication.java
@@ -0,0 +1,13 @@
+package com.chatchatabc.chatabc.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatabcAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatabcAdminApplication.class, args);
+    }
+
+}
