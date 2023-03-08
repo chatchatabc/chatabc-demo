6e36d9bfd3597ea759b3793ccb35c5710c5f73ef
Libing Chen <libing.chen@gmail.com>
2023-03-08 15:41:07 +0800

    app: ChatabcServiceApplication

---
+++ chatabc-service/src/main/java/com/chatchatabc/chatabc/ChatabcServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chatchatabc.chatabc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatabcServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatabcServiceApplication.class, args);
+    }
+
+}
